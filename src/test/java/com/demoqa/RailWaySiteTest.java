30f52d41d99185eff123ca0d530615af19720c6c
AGordey <gordey.andrej@gmail.com>
2022-04-16 23:42:00 +0300

    HW almost done

--- src/test/java/com/demoqa/RailWaySiteTest.java
+++ src/test/java/com/demoqa/RailWaySiteTest.java
@@ -8,9 +8,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -55,9 +59,30 @@ public class RailWaySiteTest {
         //$(".ac_even.ac_over").click();
         $x("/html/body/div[3]/div[3]/div[3]/div/div[3]/div[2]/div/form/div[2]/div[3]/a[3]").click();
         $("input[type='submit']").click();
-        sleep(5000);
+        sleep(4000); //шакальная команда, но без нее иногда сайт не успевал прогружаться и тест падал
         $(".sch-title__title.h2").shouldHave(text(fromCity + " — " + toCity + ","));
     }
 
+
+    static Stream<Arguments> checkHaveTrailWithStream () {
+        return Stream.of (
+            Arguments.of("Барановичи","Москва"),
+            Arguments.of("Полоцк","Брест"),
+            Arguments.of("Гродно","Гомель")
+    );
+    }
+    @MethodSource ("checkHaveTrailWithStream")
+    @ParameterizedTest (name = "Маршрут из {0} в {1} через Arguments")
+    void checkHaveTrailWithStream (String fromCity, String toCity) {
+        Selenide.open("https://www.rw.by");
+        $("#acFrom").setValue(fromCity);
+        $(".ac_even.ac_over").click();
+        $("#acTo").setValue(toCity);
+        //$(".ac_even.ac_over").click();
+        $x("/html/body/div[3]/div[3]/div[3]/div/div[3]/div[2]/div/form/div[2]/div[3]/a[3]").click();
+        $("input[type='submit']").click();
+        sleep(4000); //шакальная команда, но без нее иногда сайт не успевал прогружаться и тест падал
+        $(".sch-title__title.h2").shouldHave(text(fromCity + " — " + toCity + ","));
+    }
 }
 
